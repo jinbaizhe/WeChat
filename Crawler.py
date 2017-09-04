b39ab6e1e8ddefcb2e1827283ff3f8eef6975d11
Parker <jinbaizhe@qq.com>
2017-09-04 01:40:13 -0400

    change to Python3.5

---
+++ Crawler.py
@@ -0,0 +1,196 @@
+﻿import sys, os, time, sqlite3
+import urllib, http.cookiejar
+import re
+from bs4 import BeautifulSoup
+def verify(account, passwd):
+    postData = {
+        'user': account,
+        'pass': passwd,
+        'inputCode': '1234',
+        'url': 'http://my.zust.edu.cn/login',
+    }
+    headers = {
+        'Host': 'ez.zust.edu.cn',
+        'Origin': 'https://ez.zust.edu.cn',
+        'Referer': 'https://ez.zust.edu.cn/login?url=http://my.zust.edu.cn/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+    }
+    data = urllib.parse.urlencode(postData).encode(encoding='utf-8')
+    request = urllib.request.Request('https://ez.zust.edu.cn/login', data, headers)
+    response = opener.open(request)
+    result = response.read().decode('utf-8', 'ignore')
+    pattern1 = r'<DIV id="errmsg".*?>(.*?)</DIV>'
+    pattern2 = r'<td><li>(.*?)</li></td>'
+    match1 = re.search(pattern1, result)
+    match2 = re.search(pattern2, result)
+    if match1 or match2:
+        if match1:
+            return match1.group(1)
+        else:
+            return match2.group(1)
+    return True
+def login(info):
+    postData = {
+        'user': info['username'],
+        'pass': info['password'],
+        'inputCode': '1234',
+        'url': 'http://my.zust.edu.cn/login',
+    }
+    headers = {
+        'Host': 'ez.zust.edu.cn',
+        'Origin': 'https://ez.zust.edu.cn',
+        'Referer': 'https://ez.zust.edu.cn/login?url=http://my.zust.edu.cn/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+    }
+    data = urllib.parse.urlencode(postData).encode(encoding='utf-8')
+    request = urllib.request.Request('https://ez.zust.edu.cn/login', data, headers)
+    response = opener.open(request)
+    result = response.read().decode('utf-8', 'ignore')
+    pattern = r'<DIV id="errmsg".*?>(.*?)</DIV>'
+    match = re.search(pattern, result)
+    if match:
+        print(match.group(1))
+        return False
+    return result
+def loginZHFW(info):
+    postData = {
+        'Login.Token1': info['username'],
+        'Login.Token2': info['password'],
+        'captcha': '',
+        'goto': 'http://my.zust.edu.cn.ez.zust.edu.cn/loginSuccess.portal',
+        'gotoOnFail': 'http://my.zust.edu.cn.ez.zust.edu.cn/loginFailure.portal',
+    }
+    headers = {
+        'Host': 'my.zust.edu.cn.ez.zust.edu.cn',
+        'Origin': 'http://my.zust.edu.cn.ez.zust.edu.cn',
+        'Referer': 'http://my.zust.edu.cn.ez.zust.edu.cn/login.portal',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+    }
+    data = urllib.parse.urlencode(postData).encode(encoding='utf-8')
+    request = urllib.request.Request('http://my.zust.edu.cn.ez.zust.edu.cn/userPasswordValidate.portal', data, headers)
+    response = opener.open(request)
+def loginJWXT():
+    response = opener.open('http://jwxt.zust.edu.cn.ez.zust.edu.cn/default_zzjk.aspx')
+    result = response.read().decode('utf-8')
+    pattern_info = r'<span id="xhxm">\d*\s*(\w*?)\w\w</span>'
+    match = re.search(pattern_info, result)
+    return urllib.parse.quote(str(match.group(1)))
+def getGradePage(urlName):
+    url_cjcx = 'http://jwxt.zust.edu.cn.ez.zust.edu.cn/xscj_gc.aspx?xh=' + info[
+        'username'] + '&xm=' + urlName + '&gnmkdm=N121616'
+    headers = {
+        'Host': 'jwxt.zust.edu.cn.ez.zust.edu.cn',
+        'Referer': 'http://jwxt.zust.edu.cn.ez.zust.edu.cn/xs_main.aspx?xh=' + info['username'],
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+    }
+    request = urllib.request.Request(url_cjcx, headers=headers)
+    response = opener.open(request)
+    result = response.read().decode()
+    pattern_viewstate = r'<input type="hidden" name="__VIEWSTATE" value="(.*?)"'
+    viewstate = re.search(pattern_viewstate, result).group(1)
+
+    postData_cjcx = {
+        '__VIEWSTATE': viewstate,
+        'ddlXN': year,
+        'ddlXQ': term,
+        'Button1': '按学期查询',
+    }
+    headers_cjcx = {
+        'Host': 'jwxt.zust.edu.cn.ez.zust.edu.cn',
+        'Origin': 'http://jwxt.zust.edu.cn.ez.zust.edu.cn',
+        'Referer': url_cjcx,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+    }
+    data_cjcx = urllib.parse.urlencode(postData_cjcx).encode()
+    request_cjcx = urllib.request.Request(url_cjcx, data_cjcx, headers_cjcx)
+    response = opener.open(request_cjcx)
+    return response.read().decode('utf-8')
+def parseGradesPage(result_cjcx):
+    pattern_table = r'<table class="datelist" cellspacing="0" cellpadding="3" border="0" id="Datagrid1" width="100%">(.*?)</table>'
+    # pattern_tableHead = r'<tr class="datelisthead">(.*?)</tr>'
+    pattern_subjectItem = r'(?:<tr>|<tr class="alt">)(.*?)</tr>'
+    pattern_subjectInfo = r'<td>(.*?)</td>'
+
+    table = re.search(pattern_table, result_cjcx, re.DOTALL).group()
+    # tableHead = re.search(pattern_tableHead, table, re.DOTALL).group()
+    subjectList = list()
+    table = re.sub(r'&nbsp;', ' ', table)
+    for item in re.findall(pattern_subjectItem, table, re.DOTALL):
+        subjectList.append(re.findall(pattern_subjectInfo, item))
+    return subjectList
+def getGrade(urlName):
+    gradePage = getGradePage(urlName)
+    subjectList = parseGradesPage(gradePage)
+    return subjectList
+def getCardInfo():
+    opener.open('http://ecard.zust.edu.cn.ez.zust.edu.cn/zghyportalHome.action', timeout=30)
+    headers = {
+        'Host': 'ecard.zust.edu.cn.ez.zust.edu.cn',
+        'Referer': 'http://ecard.zust.edu.cn.ez.zust.edu.cn/accleftframe.action',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+    }
+    request = urllib.request.Request('http://ecard.zust.edu.cn.ez.zust.edu.cn/accountcardUser.action', headers=headers)
+    response = opener.open(request)
+    result = response.read().decode('utf-8', 'ignore')
+    pattern = r'余&nbsp;&nbsp;&nbsp;&nbsp;额.*?<td class="neiwen">(.*?)</td>'
+    match = re.search(pattern, result, re.DOTALL)
+    return match.group(1)
+def getLibraryInfo():
+    opener.open('http://my.lib.zust.edu.cn.ez.zust.edu.cn/idstar.aspx', timeout=30)
+    headers = {
+        'Host': 'my.lib.zust.edu.cn.ez.zust.edu.cn',
+        'Referer': 'http://my.lib.zust.edu.cn.ez.zust.edu.cn/Borrowing.aspx',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+    }
+    request = urllib.request.Request('http://my.lib.zust.edu.cn.ez.zust.edu.cn/Borrowing.aspx', headers=headers)
+    response = opener.open(request)
+    result = response.read().decode('utf8')
+    soup = BeautifulSoup(result, 'html.parser')
+    table = str(soup.find('table', id="ctl00_ContentPlaceHolder1_GridView1"))
+    pattern = r'<table.*?<a.*?>(.*?)<.*?借书时间.*?>(.*?)<.*?应还日期.*?>(.*?)<.*?续借次数.*?>(.*?)<.*?超期情况.*?>(.*?)<.*?</table>'
+    match = re.findall(pattern, table, re.DOTALL)
+    return match
+def invoke(openid, account, passwd):
+    db_path = 'info.db'
+    conn = sqlite3.connect(db_path)
+    conn.text_factory = str
+    cursor = conn.cursor()
+    info['username'] = account
+    info['password'] = passwd
+    login(info)
+    loginZHFW(info)
+
+    # '成绩':
+    try:
+        urlName = loginJWXT()
+        subjectList = getGrade(urlName)
+        cursor.execute('delete from grade where account=?',(account,))
+        for subject in subjectList:
+            cursor.execute('insert into grade values(?,?,?,?,?)',(account, subject[3], subject[6], subject[7], subject[8]))
+    except:
+        pass
+
+    # '图书馆':
+    bookList = getLibraryInfo()
+    cursor.execute('delete from library where account=?', (account,))
+    for book in bookList:
+        cursor.execute('insert into library values(?,?,?,?,?,?)',(account, book[0], book[1], book[2], book[3], book[4]))
+
+    # '一卡通':
+    try:
+        card = getCardInfo()
+    except:
+        card = '一卡通查询失败'
+    cursor.execute('delete from user where openid=?', (openid,))
+    cursor.execute('insert into user (openid,account,passwd,card,update_time,is_valid) values (?,?,?,?,?,?)',(openid, account, passwd, card,time.strftime('%Y-%m-%d-%H-%M',time.localtime(time.time())) ,'True'))
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+cookie = http.cookiejar.CookieJar()
+handler = urllib.request.HTTPCookieProcessor(cookie)
+opener = urllib.request.build_opener(handler)
+year = '2016-2017'
+term = '2'
+urlName = ''
+info = dict()
---
+++ wechat/views.py
@@ -0,0 +1,117 @@
+﻿import sqlite3,re
+import os,threading
+from django.http.response import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+from wechat_sdk import WechatConf
+from wechat_sdk import WechatBasic
+from wechat_sdk.exceptions import ParseError
+from wechat_sdk.messages import (TextMessage, VoiceMessage, ImageMessage, VideoMessage, LinkMessage, LocationMessage, EventMessage, ShortVideoMessage)
+import Crawler
+
+conf = WechatConf(
+    token='parker',
+    appid='wx2fb280445fa5d7d3',
+    appsecret='6ef28e9c05653e17ae5a822bfaef110f',
+    encrypt_mode='normal',
+    encoding_aes_key='nRP1YWs5bhUP0XjsaWLwwlpdzCYKrMEJhGnHQbn0hXN'
+)
+
+@csrf_exempt
+def WeChat(request):
+    signature = request.GET.get('signature')
+    timestamp = request.GET.get('timestamp')
+    nonce = request.GET.get('nonce')
+    wechat_instance = WechatBasic(conf=conf)
+    if not wechat_instance.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
+        return HttpResponseBadRequest('Verify Failed')
+    else:
+        if request.method == 'GET':
+           response = request.GET.get('echostr', 'error')
+        else:
+            try:
+                wechat_instance.parse_data(request.body)
+                message = wechat_instance.get_message()
+                if isinstance(message, TextMessage):
+                    db_path = 'info.db'
+                    keywords=message.content
+                    openid=message.source
+                    reply_text = ''
+                    if not os.path.isfile(db_path):
+                        conn = sqlite3.connect(db_path)
+                        cursor = conn.cursor()
+                        cursor.execute('create table user(openid varchar(30) primary key,account varchar(20),passwd varchar(20),card text,update_time varchar(20),is_valid varchar(10))')
+                        cursor.execute('create table grade(account varchar(20) ,course varchar(30),credit varchar(10),gpa varchar(10),score varchar(10))')
+                        cursor.execute('create table library(account varchar(20) ,book_name text,borrow_time varchar(20),return_time varchar(20),renew_count varchar(10),situation varchar(10))')
+                        cursor.close()
+                        conn.commit()
+                        conn.close()		    
+                    match = re.search(r'绑定\s+(\d+)\s+(.*)',keywords)
+                    if match:
+                        account=match.group(1)
+                        passwd=match.group(2)
+                        status=Crawler.verify(account, passwd)
+                        if status == True:
+                            conn=sqlite3.connect(db_path)
+                            cursor=conn.cursor()
+                            cursor.execute('select account,passwd from user where openid=?',(openid,))
+                            value=cursor.fetchall()
+                            if value:
+                                cursor.execute('update user set account=?,passwd=? where openid=?',(account,passwd,openid))
+                                reply_text ='身份验证通过，数据库大约将在1小时之后更新完成'
+                            else:
+                                t = threading.Thread(target=Crawler.invoke, args=(openid, account, passwd))
+                                t.start()
+                                reply_text = '身份验证通过，数据库大约将在1分钟之后更新完成'
+                            cursor.close()
+                            conn.commit()
+                            conn.close()
+                        else:
+                            reply_text = '身份验证未通过，错误信息：'+str(status)
+                    else:
+                        conn = sqlite3.connect(db_path)
+                        cursor = conn.cursor()
+                        cursor.execute('select * from user where openid=?', (openid,))
+                        value = cursor.fetchall()
+                        if value and (value[0][5]=='True'):
+                            account = value[0][1]
+                            update_time=value[0][4]
+                            if keywords in ['成绩','grade','Grade']:
+                                cursor.execute('select course,credit,gpa,score from grade where account=?', (account,))
+                                value = cursor.fetchall()
+                                for course in value:
+                                    reply_text += '  '.join(course)+'\r\n'
+                                reply_text +='更新时间：'+update_time
+                            elif keywords in ['图书馆','library','Library']:
+                                cursor.execute('select book_name,return_time,renew_count,situation from library where account=?', (account,))
+                                value = cursor.fetchall()
+                                for book in value:
+                                    reply_text += '  '.join(book) + '\r\n'
+                                reply_text += '更新时间：' + update_time
+                            elif keywords in ['一卡通','card','Card']:
+                                reply_text = value[0][3]+ '\r\n'
+                                reply_text += '更新时间：' + update_time
+                            else:
+                                reply_text='指令无效'
+                        else:
+                            reply_text = '账号未绑定或账号信息已过期\r\n回复 绑定 学号 统一身份认证密码（各部分用空格隔开）完成账号绑定'
+                        cursor.close()
+                        conn.commit()
+                        conn.close()
+                elif isinstance(message, VoiceMessage):
+                    reply_text = 'voice'
+                elif isinstance(message, ImageMessage):
+                    reply_text = 'image'
+                elif isinstance(message, LinkMessage):
+                    reply_text = 'link'
+                elif isinstance(message, LocationMessage):
+                    reply_text = 'location'
+                elif isinstance(message, VideoMessage):
+                    reply_text = 'video'
+                elif isinstance(message, ShortVideoMessage):
+                    reply_text = 'shortvideo'
+                else:
+                    reply_text = 'other'
+                response = wechat_instance.response_text(content=reply_text)
+            except ParseError:
+                return HttpResponseBadRequest('Invalid XML Data')
+        return HttpResponse(response, content_type="application/xml")
